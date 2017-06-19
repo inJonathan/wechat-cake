23f15d4e08dafd5fde118141d29ce11e8c09a5cd
Jonathan_Cho <jonrcao@gmail.com>
2017-06-19 23:26:35 +0800

    ud

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,32 @@
 var Zan = require('../../dist/index');
+var Data = require('../../data.js');
 
 Page(Object.assign({}, Zan.Tab, {
   data: {
-    tab1: {
-      list: [{
-        id: 'jip',
-        title: '鸡排'
-      }, {
-        id: 'bingy',
-        title: '冰饮'
-      }, {
-        id: 'taoc',
-        title: '套餐'
-      }],
-      selectedId: 'jip',
-      scroll: false
-    }
+    isLoading: true,
+    index_tab: {}
   },
   onLoad: function () {
-    
+    setTimeout(() => {
+      this.setData({
+        isLoading: false
+      });
+    }, 1000);
+    let goods = [];
+    for (let i in Data.type) {
+      let good = {};
+      good.id = Data.type[i].tid;
+      good.title = Data.type[i].name;
+      goods.push(good);
+    }
+    this.setData({
+      index_tab: {
+        list: goods,
+        selectedId: 1,
+        scroll: false
+      }
+    });
+
   },
   handleZanTabChange(e) {
     var componentId = e.componentId;
