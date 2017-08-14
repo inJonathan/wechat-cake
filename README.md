097f558cc306290f2f663ca8b16ffba74d77b5c5
Jonathan_Cho <jonrcao@gmail.com>
2017-08-15 00:49:21 +0800

    ud rm

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ## 微信小程序-仿有赞,商城模板  
 
-![](http://oiivm9udq.bkt.clouddn.com/demo/cakejpg01.jpg)![](http://oiivm9udq.bkt.clouddn.com/demo/cakejpg02.jpg)  
-![](http://oiivm9udq.bkt.clouddn.com/demo/cakejpg03.jpg)![](http://oiivm9udq.bkt.clouddn.com/demo/cakejpg04.jpg)  
+![](http://oiivm9udq.bkt.clouddn.com/xcx/cakejpg05.jpg)![](http://oiivm9udq.bkt.clouddn.com/xcx/cakejpg06.jpg)  
+![](http://oiivm9udq.bkt.clouddn.com/xcx/cakejpg07.jpg)![](http://oiivm9udq.bkt.clouddn.com/xcx/cakejpg08.jpg)  
   
-[首页演示GifDemo](http://oiivm9udq.bkt.clouddn.com/demo/cake-index.gif)  
-[详情页演示GifDemo](http://oiivm9udq.bkt.clouddn.com/demo/cake-detail.gif)  
-[购物车演示GifDemo](http://oiivm9udq.bkt.clouddn.com/demo/cake-cart.gif)  
+[演示DEMO](http://oiivm9udq.bkt.clouddn.com/xcx/cakeVideo.mp4)  
   
-PS:项目中带有zanui示例文件，占用空间较大，实际开发可以去掉
+PS:  
+项目中带有zanui示例文件，占用空间较大，实际开发可以去掉。  
+纯模板，未涉及后端。因为购物车做缓存要获取用户opendID来储存，由于没有后端就没有做，实际开发可以自行加入。
+
--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 App({
   globalData: {
-    selectGoods: [
-      // {
-      //   "gid": 1,
-      //   "kid": 1,
-      //   "count": 3,
-      //   "stock": 18,
-      //   "checked": true
-      // }
-    ]
+    selectGoods: []
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo);
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function (res) { // 获取用户openid
+          let code = res.code;
+          wx.getUserInfo({
+            success: function (res) {
+              let r_data = {};
+              r_data.code = code;
+              r_data.encryptedData = res.encryptedData;
+              r_data.iv = res.iv;
+              // wx.request({
+              //   url: 'https://xxx',
+              //   header: { "content-type": "application/x-www-form-urlencoded" },
+              //   method: 'post',
+              //   data: r_data,
+              //   success: function (res) {
+              //     console.log(res)
+              //   },
+              //   fail: function (res) {
+              //     wx.showModal({
+              //       title: '提示',
+              //       content: 'error:网络请求失败',
+              //       showCancel: false
+              //     })
+              //     return false;
+              //   }
+              // });
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo);
+            }
+          });
+        }
+      });
+    }
   }
 });
